2441a72896837e69bad45d51fedc54216f62c8b0
Mazy ElMasry <mazymelmasry@gmail.com>
2025-07-08 21:45:45 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,20 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Book b1 = new Paperbook("A11", "MATH", 2020, 67, 20);
-        Book b2 = new EBook("A12", "science", 2024, 54, "pdf");
-        Book b3 = new Paperbook("A14", "OLD MATH", 2007, 67, 10);
-        Book b4 = new EBook("A13", "OLD SCIENCE", 2004, 54, "pdf");
+        Book b1 = new Paperbook("b1", "MATH", 2024, 20, 200);
+        Book b2 = new EBook("b2", "Physics", 2021, 50, "pdf");
+        Book b3 = new Paperbook("b3", "Biology", 2020, 100, 120);
+        Book b4 = new EBook("b4", "English", 2004, 150, "pdf");
 
         Inventory inventory = new Inventory();
 
-        inventory.addBook(b1, 20); // Even if the book already stores stock
-        inventory.addBook(b2, 10);
-        inventory.addBook(b3, 15);
-        inventory.addBook(b4, 5);
+        inventory.addBook(b1, 4);
+        inventory.addBook(b2, 6);
+        inventory.addBook(b3, 12);
+        inventory.addBook(b4, 1);
 
         System.out.println("\n--- Purchasing Book ---");
-        b1.purchaseBook("A11", 2, "mazen@gmail.com", "Smouha");
+        b1.purchaseBook("b1", 2, "Mazy@mail.com", "Alex");
 
         System.out.println("\n--- Removing Books Published Before 2010 ---");
         inventory.removeOutdatedBooks(2010);
